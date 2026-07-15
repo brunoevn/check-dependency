06789106c96a056e7e5396d41169b95fd59ab559
Bruno Nielsen <brunovn@gmail.com>
2026-07-15 00:05:11 -0300

    chore: bump version to 1.9.5

--- kevlar.py
+++ kevlar.py
@@ -44,7 +44,7 @@ class SafeWriter:
 sys.stdout = SafeWriter(sys.stdout)
 sys.stderr = SafeWriter(sys.stderr)
 
-VERSION = "1.9.4"
+VERSION = "1.9.5"
 
 # External APIs Configuration
 URL_NPM_REGISTRY = "https://registry.npmjs.org/"
